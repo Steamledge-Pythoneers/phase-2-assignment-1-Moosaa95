407274c2a2b2c5a226c46c000057e8be19110d22
Musa Abdullahi <moosaabdullahi45@gmail.com>
2020-09-29 17:42:09 +0100

    function to reduce to lowest terms

--- Exercise/compute_lowest_terms.py
+++ Exercise/compute_lowest_terms.py
@@ -1,12 +1,12 @@
 ## TODO: complete the function "lowest_terms" below
 ##lowest terms
 def lowest_terms(x):
-	
-x = x.split('/')
+	""" split a string into a list after the / operator """
+	x = x.split('/')
 	numerator = int(x[0])
 	denominator = int(x[1])
 
-	""" check for undefined and zeroError """
+	
 	if denominator == 0:
 		return "Undefined" 
 	elif numerator == 0:
@@ -14,6 +14,7 @@ x = x.split('/')
 	else:
 		return reduceFrac(numerator, denominator)
 	
+
 """ using recursion to get the greatest common divisor 
 	keeping it simple with Lambda
 
@@ -21,17 +22,12 @@ x = x.split('/')
 """
 gcd = lambda a,b : a if (b == 0) else gcd(b, a%b)
 
+##reduceFrac function
+## returns the reduce
+def reduceFrac(num,den):
+	x = gcd(num, den)
+	num = num // x
+	den = den // x
 
 	
-x = x.split('/')
-	numerator = int(x[0])
-	denominator = int(x[1])
-
-	
-	if denominator == 0:
-		return "Undefined" 
-	elif numerator == 0:
-		return "0"
-	else:
-		return reduceFrac(numerator, denominator)
-	
+	return "{}/{}".format(int(num), int(den))
