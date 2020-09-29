3ba1bb8c0de358cbbff3e8664b4d810c4df58b7e
Musa Abdullahi <moosaabdullahi45@gmail.com>
2020-09-29 15:09:22 +0100

    function to get numbers gcd

--- Exercise/compute_lowest_terms.py
+++ Exercise/compute_lowest_terms.py
@@ -2,3 +2,11 @@
 
 def lowest_terms(x):
 	return ""
+
+""" using recursion to get the greatest common divisor 
+	keeping it simple with Lambda
+
+	this collects integer a and integer b and returns their greatest common divisor
+"""
+gcd = lambda a,b : a if (b == 0) else gcd(b, a%b)
+
