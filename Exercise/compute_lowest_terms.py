d12bdf8246f4c6103bbd25c833ed089caa7f17c6
Musa Abdullahi <moosaabdullahi45@gmail.com>
2020-09-30 13:55:55 +0100

    correction of docstrings

--- Exercise/compute_lowest_terms.py
+++ Exercise/compute_lowest_terms.py
@@ -1,12 +1,16 @@
 ## TODO: complete the function "lowest_terms" below
 ##lowest terms
 def lowest_terms(x):
-	""" split a string into a list after the / operator """
+	""" 
+		compute and return the lowest term of a fraction
+		split  (x string) into a list after the / operator 
+		
+	"""
 	x = x.split('/')
 	numerator = int(x[0])
 	denominator = int(x[1])
 
-	
+	#check for zeros and undefined
 	if denominator == 0:
 		return "Undefined" 
 	elif numerator == 0:
@@ -15,16 +19,16 @@ def lowest_terms(x):
 		return reduceFrac(numerator, denominator)
 	
 
-""" using recursion to get the greatest common divisor 
-	keeping it simple with Lambda
-
-	this collects integer a and integer b and returns their greatest common divisor
-"""
+##using recursion to return the greatest common divisor of a and b
 gcd = lambda a,b : a if (b == 0) else gcd(b, a%b)
 
-##reduceFrac function
-## returns the reduce
+
 def reduceFrac(num,den):
+	""" takes in two numbers and returns the reduce form
+	num (int) - numerator
+	den (int) - denominator
+	call the gcd function
+	"""
 	x = gcd(num, den)
 	num = num // x
 	den = den // x
